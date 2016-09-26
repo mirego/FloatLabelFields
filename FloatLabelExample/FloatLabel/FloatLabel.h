268c8c9fd9f81e0575a6c9b8784727c941d7d375
Felix Lapalme <lap.felix@gmail.com>
2016-09-26 14:54:24 -0400

    Make it work with Carthage

---
+++ FloatLabelExample/FloatLabel/FloatLabel.h
@@ -0,0 +1,19 @@
+//
+//  FloatLabel.h
+//  FloatLabel
+//
+//  Created by Felix Lapalme on 2016-09-26.
+//  Copyright © 2016 RookSoft Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FloatLabel.
+FOUNDATION_EXPORT double FloatLabelVersionNumber;
+
+//! Project version string for FloatLabel.
+FOUNDATION_EXPORT const unsigned char FloatLabelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FloatLabel/PublicHeader.h>
+
+
